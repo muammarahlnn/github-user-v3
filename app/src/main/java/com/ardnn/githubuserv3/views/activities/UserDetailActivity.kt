c94d293391c721815e722d4243afdcbc58d7075f
muammarahlnn <21muammarahlan@gmail.com>
2021-11-14 21:31:17 +0800

    add user detail's UI

---
+++ app/src/main/java/com/ardnn/githubuserv3/views/activities/UserDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.ardnn.githubuserv3.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ardnn.githubuserv3.R
+
+class UserDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+    }
+}
