5af539bda0dabc2bd4712ecf950c6359c00578ef
muammarahlnn <21muammarahlan@gmail.com>
2021-11-20 15:48:38 +0800

    add favorite user repository

---
+++ app/src/main/java/com/ardnn/githubuserv3/database/FavoriteUserRepository.kt
@@ -0,0 +1,29 @@
+package com.ardnn.githubuserv3.database
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class FavoriteUserRepository(application: Application) {
+    private val favoriteUserDao: FavoriteUserDao =
+        FavoriteUserDatabase.getDatabase(application).getFavoriteUserDao()
+    private val executorService: ExecutorService =
+        Executors.newSingleThreadExecutor()
+
+    fun getAllFavoriteUsers(): LiveData<List<FavoriteUser>> {
+        return favoriteUserDao.getAllFavoriteUsers()
+    }
+
+    fun insert(favoriteUser: FavoriteUser) {
+        executorService.execute {
+            favoriteUserDao.insert(favoriteUser)
+        }
+    }
+
+    fun delete(favoriteUser: FavoriteUser) {
+        executorService.execute {
+            favoriteUserDao.delete(favoriteUser)
+        }
+    }
+}
